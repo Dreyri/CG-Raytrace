27d536691ec7ad14ca2111e063edac0ef0b742b7
Frederik <frederik.engels24@gmail.com>
2018-05-31 14:52:26 +0200

    add basic qt support

--- src/example.cpp
+++ src/example.cpp
@@ -1,6 +1,12 @@
 #include <assimp/scene.h>
 #include <glm/glm.hpp>
 
+#include <QWindow>
+
 int main(int argc, char** argv) {
   aiScene example;
+
+  QWindow* example_window = new QWindow();
+
+  delete example_window;
 }
