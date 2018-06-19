8a5ed395db93c9c6c7ca2fc5fd0fcf63b6195d64
Frederik <frederik.engels24@gmail.com>
2018-06-19 17:35:35 +0200

    trace a simple sphere, currently still using orthogonal projection

--- src/core/tracer_sphere.cpp
+++ src/core/tracer_sphere.cpp
@@ -1,5 +1,7 @@
 #include "tracer_sphere.hpp"
 
+#include "camera.hpp"
+
 namespace rt {
 glm::vec3 tracer_sphere::cast(const rt::ray& r, const rt::scene& scene) const {
   glm::vec3 color = scene.background_color;
@@ -14,7 +16,40 @@ glm::vec3 tracer_sphere::cast(const rt::ray& r, const rt::scene& scene) const {
   return color;
 }
 
-std::vector<glm::vec3> render(const rt::scene& scene) const;
+std::vector<glm::vec3> tracer_sphere::render(const rt::scene& scene) const {
+  int width{1920};
+  int height{1080};
+  int halfwidth = width / 2;
+  int halfheight = height / 2;
+
+  std::vector<glm::vec3> res;
+  res.reserve(width * height);
+  std::vector<rt::ray> rays;
+  rays.reserve(width * height);
+
+  rt::camera cam{{0.0f, 0.0f, -20.0f}, glm::quat(), 60.0f};
+
+  // generate rays, probably put this in a separate function
+  for (size_t i = 0; i < width * height; ++i) {
+    int xoffset = i % width;
+    int yoffset = i / width;
+
+    glm::vec3 direction = {0.0f, 0.0f, 1.0f};
+    glm::vec3 position = {xoffset - halfwidth - cam.position.x,
+                          yoffset - halfheight - cam.position.y,
+                          cam.position.z};
+    direction = cam.rotation * direction;
+
+    rt::ray r{position, direction};
+    rays.push_back(r);
+  }
+
+  for (const auto& ray : rays) {
+    res.push_back(cast(ray, scene));
+  }
+
+  return res;
+}
 
 trace_data tracer_sphere::trace(const rt::ray& r,
                                 const rt::scene& scene) const {
--- src/example.cpp
+++ src/example.cpp
@@ -1,28 +1,42 @@
 #include <memory>
 #include <vector>
 
+#include <QImage>
+#include <QWindow>
 #include <assimp/scene.h>
 #include <glm/glm.hpp>
-#include <QWindow>
-#include <QImage>
 
 #include "core/raytracer_simple.hpp"
+#include "core/tracer_sphere.hpp"
 
-int main(int argc, char** argv)
-{
-    rt::RaytracerSimple tracer = rt::RaytracerSimple();
-    auto rgbbuffer = tracer.render();
+int main(int argc, char** argv) {
+  rt::RaytracerSimple tracer = rt::RaytracerSimple();
+  auto rgbbuffer = tracer.render();
 
-    QImage image = QImage(1920, 1080, QImage::Format::Format_RGB888);
+  QImage image = QImage(1920, 1080, QImage::Format::Format_RGB888);
 
-    for (unsigned int h = 0; h < 1080; h++)
-    {
-        for (unsigned int w = 0; w < 1920; w++)
-        {
-            glm::uvec3 color = (*rgbbuffer)[h][w];
-            image.setPixelColor(w, h, QColor(color.r, color.g, color.b));
-        }
+  for (unsigned int h = 0; h < 1080; h++) {
+    for (unsigned int w = 0; w < 1920; w++) {
+      glm::uvec3 color = (*rgbbuffer)[h][w];
+      image.setPixelColor(w, h, QColor(color.r, color.g, color.b));
     }
+  }
 
-    image.save("example.png", "PNG");
+  image.save("example.png", "PNG");
+
+  rt::scene simple_scene;
+  simple_scene.add_sphere({{0.0f, 0.0f, 0.0f}, 15.0f});
+  rt::tracer_sphere sphere_raytracer;
+
+  auto image_data = sphere_raytracer.render(simple_scene);
+  std::vector<glm::tvec4<uchar>> converted_image_data(std::size(image_data));
+
+  std::transform(std::begin(image_data), std::end(image_data),
+                 std::begin(converted_image_data), [](const auto& v) -> glm::tvec4<uchar> {
+                   return {v.x * 255.0f, v.y * 255.0f, v.z * 255.0f, 255};
+                 });
+
+  QImage sphere_image(reinterpret_cast<uchar*>(std::data(converted_image_data)), 1920, 1080, QImage::Format_RGBA8888, nullptr, nullptr);
+
+  sphere_image.save("example_sphere.png", "PNG");
 }
