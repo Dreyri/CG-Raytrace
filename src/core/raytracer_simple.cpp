59841be83a041daca5dbc45feb122514769dfe73
Umorrian <Arne.Steen@gmx.de>
2018-06-19 21:50:56 +0200

    local light model, still bugged

--- src/core/raytracer_simple.cpp
+++ src/core/raytracer_simple.cpp
@@ -1,11 +1,38 @@
 #include "raytracer_simple.hpp"
 
+#include <iostream>
+
 using namespace rt;
+
+struct Light
+{
+    glm::vec3 position;
+    glm::vec3 color;
+    float intensity;
+};
+
+struct AmbientLight
+{
+    glm::vec3 color;
+    float intensity;
+};
+
+struct Material
+{
+    glm::vec3 ambient;
+    glm::vec3 diffuse;
+    glm::vec3 specular;
+    float shininess;
+
+    bool smoothed;
+};
 
 struct Polygon
 {
     glm::vec3 v1, v2, v3;
-    glm::uvec3 color;
+    glm::vec3 normal;
+
+    std::shared_ptr<Material> material;
 };
 
 struct Camera
@@ -84,6 +111,67 @@ bool RayIntersectsTriangle(glm::vec3 rayOrigin, glm::vec3 rayVector, Polygon inT
         return false;
 }
 
+bool lightVisible(std::shared_ptr<std::vector<Polygon>> polygons, Light light, ray lightRay)
+{
+    glm::vec3* i = new glm::vec3();
+    for (auto const poly : *polygons)
+    {
+        if (RayIntersectsTriangle(lightRay.origin, lightRay.direction, poly, i))
+        {
+            delete i;
+            return false;
+        }
+    }
+    delete i;
+    return true;
+}
+
+glm::vec3 localLight(std::shared_ptr<std::vector<Polygon>> polygons, unsigned int polyIndex, Light light, AmbientLight ambient, ray viewRay, glm::vec3 intersection)
+{
+    Polygon poly = (*polygons)[polyIndex];
+
+    glm::vec3 normal;
+    if (poly.material->smoothed)
+    {
+        // Normale interpolieren
+    }
+    else
+    {
+        normal = poly.normal;
+    }
+
+    glm::vec3 lightVector = glm::normalize(light.position - intersection);
+    glm::vec3 reflectVector = glm::normalize(2 * glm::dot(lightVector, normal) * normal - lightVector);
+    glm::vec3 viewVector = glm::normalize(viewRay.origin - intersection);
+
+    glm::vec3 a, d, s;
+    a = poly.material->ambient * (ambient.intensity * ambient.color);
+
+    float theta = glm::dot(lightVector, normal);
+    if (theta >= 0.0f && lightVisible(polygons, light, ray(intersection, lightVector)))
+    {
+        d = poly.material->diffuse * theta * (light.intensity * light.color);
+
+        float alpha = glm::dot(reflectVector, viewVector);
+        if (alpha >= 0.0f)
+        {
+            s = poly.material->specular * glm::pow(alpha, poly.material->shininess) * (light.intensity * light.color);
+            float x = glm::dot(reflectVector, viewVector);
+            float y = glm::pow(x, poly.material->shininess);
+
+            return a + d + s;
+        }
+        else
+        {
+            return a + d;
+        }
+    }
+    else
+    {
+        return a;
+    }
+}
+
 std::unique_ptr<std::vector<std::vector<glm::uvec3>>> RaytracerSimple::render()
 {
     unsigned int height = 1080;
@@ -91,50 +179,71 @@ std::unique_ptr<std::vector<std::vector<glm::uvec3>>> RaytracerSimple::render()
 
     std::unique_ptr<std::vector<std::vector<glm::uvec3>>> image = std::make_unique<std::vector<std::vector<glm::uvec3>>>(height, std::vector<glm::uvec3>(width, {128, 128, 128}));
 
-    std::vector<glm::vec3> vertexlist{
-        {-1.0, 1.0, -1.0},
-        {-1.0, -1.0, -1.0},
+    std::vector<glm::vec3> vertexList{
+        {-1.0, 1.0, 1.0},
+        {-1.0, -1.0, 1.0},
+        {1.0, -1.0, 1.0},
+        {1.0, 1.0, 1.0},
         {1.0, -1.0, -1.0},
         {1.0, 1.0, -1.0},
-        {-1.0, -1.0, 1.0},
-        {-1.0, 1.0, 1.0},
-        {1.0, -1.0, 1.0},
-        {1.0, 1.0, 1.0}
+        {-1.0, 1.0, -1.0},
+        {-1.0, -1.0, -1.0},
     };
 
-    std::vector<glm::uvec3> indexlist {
-        {0, 1, 2},
-        {2, 3, 0},
-        {4, 1, 0},
-        {0, 5, 4},
-        {2, 6, 7},
-        {7, 3, 2},
-        {4, 5, 7},
-        {7, 6, 4},
-        {0, 3, 7},
-        {7, 5, 0},
-        {1, 4, 2},
-        {2, 4, 6}
+    std::vector<glm::vec3> normalList{
+        { 0.0, 0.0, 1.0 },
+        { 1.0, 0.0, 0.0 },
+        { 0.0, 1.0, 0.0 },
+        { 0.0, 0.0, -1.0 },
+        { -1.0, 0.0, 0.0 },
+        { 0.0, -1.0, 0.0 },
     };
 
-    glm::uvec3 mesh_color{ 255, 0, 0 };
+    std::vector<glm::uvec3> vertexIndices {
+        {1, 2, 3},
+        {1, 3, 4},
+        {4, 3, 5},
+        {4, 5, 6},
+        {4, 6, 7},
+        {4, 7, 1},
+        {6, 5, 8},
+        {8, 8, 7},
+        {1, 7, 8},
+        {1, 8, 2},
+        {3, 8, 5},
+        {3, 2, 8},
+    };
 
-    std::vector<Polygon> polygons = std::vector<Polygon>();
+    std::vector<unsigned int> normalIndixes{
+        1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6,
+    };
+
+    std::shared_ptr<Material> material = std::make_shared<Material>();
+    material->ambient = { 0.329412f, 0.223529f, 0.027451f };
+    material->diffuse = { 0.780392f, 0.568627f, 0.113725f };
+    material->specular = { 0.992157f, 0.941176f, 0.807843f };
+    material->shininess = 27.8974f;
+    material->smoothed = false;
+
+    std::shared_ptr<std::vector<Polygon>> polygons = std::make_shared<std::vector<Polygon>>(std::vector<Polygon>());
 
     glm::mat4 model_rotate = glm::rotate(glm::mat4(1.0f), glm::radians(45.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     glm::mat4 model_scale = glm::scale(glm::mat4(1.0f), glm::vec3(2.0f));
     glm::mat4 model_position = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 4.0f));
     glm::mat4 model_transform = model_position * model_scale * model_rotate;
     
-    for (auto const indices : indexlist)
+    for (unsigned int i = 0; i < vertexIndices.size(); i++)
     {
         Polygon polygon = Polygon();
-        polygon.v1 = model_transform * glm::vec4(vertexlist[indices.x], 1.0f);
-        polygon.v2 = model_transform * glm::vec4(vertexlist[indices.y], 1.0f);
-        polygon.v3 = model_transform * glm::vec4(vertexlist[indices.z], 1.0f);
-        polygon.color = mesh_color;
+        polygon.v1 = model_transform * glm::vec4(vertexList[vertexIndices[i].x - 1], 1.0f);
+        polygon.v2 = model_transform * glm::vec4(vertexList[vertexIndices[i].y - 1], 1.0f);
+        polygon.v3 = model_transform * glm::vec4(vertexList[vertexIndices[i].z - 1], 1.0f);
 
-        polygons.push_back(polygon);
+        polygon.normal = normalList[normalIndixes[i] - 1];
+
+        polygon.material = material;
+
+        polygons->push_back(polygon);
     }
 
     Camera cam = Camera();
@@ -146,6 +255,15 @@ std::unique_ptr<std::vector<std::vector<glm::uvec3>>> RaytracerSimple::render()
     cam.imageWidth = width;
     cam.fov = 90;
     cam.calculateDerived();
+
+    AmbientLight ambient = AmbientLight();
+    ambient.color = glm::uvec3(1.0f, 1.0f, 1.0f);
+    ambient.intensity = 0.5f;
+
+    Light light = Light();
+    light.position = glm::vec3(5.0f, 10.0f, 0.0f);
+    light.color = glm::uvec3(1.0f, 1.0f, 1.0f);
+    light.intensity = 1.0f;
 
     std::vector<std::vector<ray>> rays = std::vector<std::vector<ray>>(cam.imageHeight, std::vector<ray>(cam.imageWidth, ray(glm::vec3(0.0f), glm::vec3(0.0f))));
 
@@ -161,7 +279,6 @@ std::unique_ptr<std::vector<std::vector<glm::uvec3>>> RaytracerSimple::render()
 
     // ray <> triangle intersection
     glm::vec3* intersection = new glm::vec3(0.0);
-    glm::uvec3 color;
     for (unsigned int h = 0; h < cam.imageHeight; h++)
     {
         for (unsigned int w = 0; w < cam.imageWidth; w++)
@@ -169,9 +286,9 @@ std::unique_ptr<std::vector<std::vector<glm::uvec3>>> RaytracerSimple::render()
             float closestDistance = 99999999.0f;
             unsigned int closestIndex = -1;
 
-            for (unsigned int p = 0; p < polygons.size(); p++)
+            for (unsigned int p = 0; p < polygons->size(); p++)
             {
-                if (RayIntersectsTriangle(rays[h][w].origin, rays[h][w].direction, polygons[p], intersection))
+                if (RayIntersectsTriangle(rays[h][w].origin, rays[h][w].direction, (*polygons)[p], intersection))
                 {
                     float distance = glm::length(*intersection - rays[h][w].origin);
                     if (distance < closestDistance)
@@ -184,7 +301,14 @@ std::unique_ptr<std::vector<std::vector<glm::uvec3>>> RaytracerSimple::render()
 
             if (closestIndex != -1)
             {
-                (*image)[h][w] = polygons[closestIndex].color;
+                glm::vec3 ll = localLight(polygons, closestIndex, light, ambient, rays[h][w], *intersection);
+                glm::uvec3 color = glm::uvec3();
+
+                color.r = 255 * ll.r;
+                color.g = 255 * ll.g;
+                color.b = 255 * ll.b;
+
+                (*image)[h][w] = color;
             }            
         }
     }
